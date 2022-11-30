7067cd9d8046ef37f11af300e9f14e5011397834
Nguyen Quynh Anh 0036 <117666813+NguyenQuynhAnh0036@users.noreply.github.com>
2022-11-30 11:19:26 +0700

    Add files via upload

---
+++ Bai 8.2.py
@@ -0,0 +1,3 @@
+number=-10
+absolute_number=abs(number)
+print(absolute_number)
---
+++ Bai 8.3.py
@@ -0,0 +1,9 @@
+a=float(input("Nhập hệ số a:"))
+b=float(input("Nhập hệ số b:"))
+if a==0:
+    if b==0:
+        print("Phương trình vô số nghiệm")
+    else:
+        print("Phương trình vô nghiệm")
+else:    
+    print("Phương trình có nghiệm x=",-b/a)
