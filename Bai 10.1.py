a97f0a07f3d5c9c87d757f84f563621c94434c72
Nguyen Quynh Anh 0036 <117666813+NguyenQuynhAnh0036@users.noreply.github.com>
2022-12-06 21:22:27 +0700

    Add files via upload

---
+++ Bai 10.1.py
@@ -0,0 +1,4 @@
+num=[-5,0,3,19,44]
+print("Số lớn nhất trong các số trên là:",max(num))
+num=[-5,0,3,19,44]
+print("Số nhỏ nhất trong các số trên là:",min(num))
---
+++ Bai 10.2.py
@@ -0,0 +1,4 @@
+import math
+x=float(input("Nhập x:"))
+print("Giá trị tuyệt đối của a là:",end="")
+print(math.fabs(x))
